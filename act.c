0d646c750480407efea4bcad4d177bad74dca7ec
Gucchi3 <nnykb-k@outlook.jp>
2026-01-09 17:33:56 +0900

    Add activation functions: RELU, RELU6, and SiLU implementations

---
+++ act.c
@@ -0,0 +1,51 @@
+#include "Tensor.h"
+#include <math.h>
+
+
+Tensor* RELU(Tensor* input_tensor){
+  for(int i=0; i < input_tensor->H * input_tensor->W * input_tensor->C; i++){
+    input_tensor->data[i] = (input_tensor->data[i] < 0) ?  0 : input_tensor->data[i]  ;
+  }
+  return input_tensor;
+}
+
+
+Tensor* RELU6(Tensor* input_tensor){
+  for(int i=0; i < input_tensor->H * input_tensor->W * input_tensor->C; i++){
+    input_tensor->data[i] = (input_tensor->data[i] < 0) ?  0 : ((input_tensor->data[i] > 6) ? 6 : input_tensor->data[i]);
+    
+  }
+  return input_tensor;
+}
+
+
+Tensor* SiLU(Tensor* input_tensor){
+    for(int i=0; i < input_tensor->H * input_tensor->W * input_tensor->C; i++){
+    input_tensor->data[i] =  input_tensor->data[i] / (1 + exp(- input_tensor->data[i])); 
+  }
+  return input_tensor;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ act.h
@@ -0,0 +1,33 @@
+#include "Tensor.h"
+
+
+
+/*----------------------------------------------------
+RELU：RELU関数
+must：input_tensor
+
+explanation：
+　Tensor->dataの0未満のものを0にクリップする。
+------------------------------------------------------*/
+Tensor* RELU(Tensor* input_tensor);
+
+
+/*----------------------------------------------------
+RELU6：RELU6関数
+must：input_tensor
+
+explanation：
+　Tensor->dataの0未満のものを0に、6以上のものを6にクリップする。
+------------------------------------------------------*/
+Tensor* RELU6(Tensor* input_tensor);
+
+
+
+/*----------------------------------------------------
+SiLU：SiLU関数
+must：input_tensor
+
+explanation：
+x = x / (1 + e^(-x)) を提供します。
+------------------------------------------------------*/
+Tensor* SiLU(Tensor* input_tensor);
